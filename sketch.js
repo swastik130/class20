dc4ec5b48bd41eaa2df39e356ac471a859d35262
swastik130 <77870796+swastik130@users.noreply.github.com>
2021-01-28 08:14:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var fixedRect, movingRect;
+
+
+function setup() {
+  createCanvas(800,400);
+
+  fixedRect = createSprite(600, 200, 50, 80);
+  fixedRect.shapeColor = "green";
+
+ movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+ 
+}
+
+function draw() {
+  background(255,255,255);  
+
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 &&
+    fixedRect.x - movingRect.x < movingRect.width/2+fixedRect.width/2 &&
+    movingRect.y - fixedRect.y < fixedRect.width/2 + movingRect.width/2 &&
+    fixedRect.y - movingRect.y < movingRect.width/2+fixedRect.width/2  ){
+    fixedRect.shapeColor = "red";
+    movingRect.shapeColor = "red";
+  }
+
+  else {
+    fixedRect.shapeColor = "green";
+    movingRect.shapeColor = "green";
+  }
+
+  drawSprites();
+}
